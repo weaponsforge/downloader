60c424241878b6e6e15476ab1537cc6647f224f4
weaponsforge <forgeweapons@gmail.com>
2020-09-17 18:32:52 +0800

    Write dl urls to file.

--- downloader.js
+++ downloader.js
@@ -3,27 +3,64 @@ const fs = require('fs')
 const path = require('path')
 const exec = require('child_process').exec
 const parser = require('fast-xml-parser')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+const { document } = (new JSDOM()).window
 
-const readDownloadList = async () => {
-  // const list = fs.readFileSync(path.join(__dirname, '/list.txt'), 'utf8')
-  // const dlList = list.split('\n')
+const execDownload = (cmd) => {
+  exec(cmd, (err, data) => {
+    if (err) {
+      console.log(`Download error: ${err.message}`)
+    } else {
+      console.log('DONE')
+    }
+  })
+}
+
+/**
+ * Return a list of download urls in newline-delimited strings from datasets in
+ * http://datasets.pacb.com.s3.amazonaws.com
+ * @param {String} downloadUrl
+ * @returns {String} download urls
+ */
+const getDownloadURLS = async (downloadUrl) => {
+  let urls = ''
+  const a = document.createElement('a')
+  a.href = downloadUrl
+  const baseUrl = `${a.protocol}//${a.hostname}`
 
   try {
-    const xml = await axios.get('http://datasets.pacb.com.s3.amazonaws.com/?prefix=2014/Arabidopsis/raw')
+    const xml = await axios.get(downloadUrl)
     const obj = parser.parse(xml.data, true)
     const cmd = `wget -c http://datasets.pacb.com.s3.amazonaws.com/${obj.ListBucketResult.Contents[0].Key}`
-    console.log(`Downloading ${obj.ListBucketResult.Contents[0].Size} B\n${cmd}`)
 
-    exec(cmd, (err, data) => {
-      if (err) {
-        console.log(`Download error: ${err.message}`)
-      } else {
-        console.log('DONE')
-      }
+    obj.ListBucketResult.Contents.forEach(item => {
+      urls += `${baseUrl}//${item.Key}\n`
     })
   } catch (err) {
     console.log(err)
   }
+
+  return urls
 }
 
-readDownloadList()
+/**
+ * Write the download URL's from http://datasets.pacb.com.s3.amazonaws.com data sets to a text file
+ * @param {String} downloadUrl URL that returns an XML response of download links from 
+ * http://datasets.pacb.com.s3.amazonaws.com data sets
+ */
+const readDownloadList = async (downloadUrl) => {
+  const filePath = path.join(__dirname, '/downloadlist.txt')
+  const downloadList = await getDownloadURLS(downloadUrl)
+
+  try {
+    fs.writeFile(filePath, downloadList, (err) => {
+      if (err) console.log(`Error: ${err.message}`)
+      console.log('File created.')
+    })
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+readDownloadList('http://datasets.pacb.com.s3.amazonaws.com/?prefix=2014/Arabidopsis/raw')
